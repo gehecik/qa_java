e9ac705ebb8e5db0dc0a09edeed6db217f57761a
Kechek Karine <kechek.karine@gmail.com>
2026-03-08 19:16:39 +0300

    add getKittensMaleTest

--- src/test/java/com/exemple/LionTest.java
+++ src/test/java/com/exemple/LionTest.java
@@ -1,4 +1,26 @@
 package com.exemple;
 
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
 public class LionTest {
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getKittensMaleTest() throws Exception{
+        Lion lion = new Lion("Самец", feline);
+
+        Mockito.when(feline.getKittens()).thenReturn(5);
+        assertEquals(5, lion.getKittens());
+    }
+
 }
