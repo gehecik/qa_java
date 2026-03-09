3887a7d6eb6aecf098c18fec0d95a2f297a2c43d
Kechek Karine <kechek.karine@gmail.com>
2026-03-10 00:08:20 +0300

    add getFoodExceptionTest for class Animal

--- src/test/java/com/exemple/AnimalTest.java
+++ src/test/java/com/exemple/AnimalTest.java
@@ -1,15 +1,33 @@
 package com.exemple;
 
 import com.example.Animal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AnimalTest {
+    @Test
+    public void getFoodExceptionTest() {
+        getFoodExceptionTest(null);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "Всеядные"})
+    public void getFoodExceptionTest(String animalKind) {
+        Animal animal = new Animal();
+
+        assertThrows(Exception.class, () -> animal.getFood(animalKind));
+    }
+
     @ParameterizedTest
     @MethodSource("animalAndFoodData")
     public void getFoodTest(String animalKind, List<String> expected) throws Exception {
