5af073af5a0cdaee9f0d874e55208a5d1dc0ff6c
Kechek Karine <kechek.karine@gmail.com>
2026-03-09 23:50:41 +0300

    add getFoodTest for class Animal

--- src/test/java/com/exemple/AnimalTest.java
+++ src/test/java/com/exemple/AnimalTest.java
@@ -1,4 +1,27 @@
 package com.exemple;
 
+import com.example.Animal;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AnimalTest {
+    @ParameterizedTest
+    @MethodSource("animalAndFoodData")
+    public void getFoodTest(String animalKind, List<String> expected) throws Exception {
+        Animal animal = new Animal();
+        List<String> food = animal.getFood(animalKind);
+
+        assertEquals(expected, food);
+    }
+    private static Stream<Arguments> animalAndFoodData() {
+        return Stream.of(
+                Arguments.of("Травоядное", List.of("Трава", "Различные растения")),
+                Arguments.of("Хищник", List.of("Животные", "Птицы", "Рыба"))
+        );
+    }
 }
