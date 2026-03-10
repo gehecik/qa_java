e1307d6484bba376bb259755dbb0ed9e81880461
Kechek Karine <kechek.karine@gmail.com>
2026-03-11 00:28:19 +0300

    add eatMeatTest

---
+++ src/test/java/com/exemple/FelineTest.java
@@ -0,0 +1,19 @@
+package com.exemple;
+
+import com.example.Feline;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FelineTest {
+
+    @Test
+    public void eatMeatTest() throws Exception {
+        Feline feline = new Feline();
+        List<String> food = List.of("Животные", "Птицы", "Рыба");
+
+        assertEquals(food, feline.eatMeat());
+    }
+}
